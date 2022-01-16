0b8146f3e6cb7de98ce829145fec00c6d4ebce52
Andreas Levander <Androz@Andreass-MacBook-Pro.local>
2022-01-16 22:23:20 +0200

    added most played logic

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -8,10 +8,32 @@ const requestProfile = async (msg, socket) => {
         return;
     }
 
+    //winrate logic
     let winrate = 0;
     if (stats.matches_played > 0 && stats.wins > 0) {
         winrate = parseInt(stats.wins/stats.matches_played * 100);
     }
+
+    let mostplayed = {
+        hand: "",
+        times: "",
+    };
+    const rocks = stats.rock_played;
+    const papers = stats.paper_played;
+    const scissors = stats.scissors_played;
+    
+    //most played hand logic
+    if(rocks >= papers && rocks >= scissors) {
+        mostplayed.hand = "ROCK";
+        mostplayed.times = rocks;
+    } else if (papers >= rocks && papers >= scissors) {
+        mostplayed.hand = "PAPER";
+        mostplayed.times = papers;
+    } else if (scissors >= rocks && scissors >= papers) {
+        mostplayed.hand = "SCISSORS";
+        mostplayed.times = scissors;
+    }
+
     const matches = await dbService.getmatchhistory(msg);
 
     const data = {
@@ -19,12 +41,13 @@ const requestProfile = async (msg, socket) => {
         name: stats.name,
         matches_played: stats.matches_played,
         winrate: winrate,
+        mostplayed: mostplayed,
         match_history : matches,
     }
     
 
     const jsonstring = JSON.stringify(data);
-    //console.log(jsonstring);
+    
     await sendMessage(socket,jsonstring);
 
 }
